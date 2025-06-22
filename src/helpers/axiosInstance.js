f500b51a6a99669eb774ccd16510d5782c276ef1
Saroj Kumar Das <sarojsarojkumar753@gmail.com>
2025-06-22 11:08:22 +0530

    Started deploying this app 1

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3300" || "https://taskflowbackend-cbxp.onrender.com", 
+  baseURL: "https://taskflowbackend-cbxp.onrender.com", 
   withCredentials: true,            //For cookies/auth
 });
 
--- src/helpers/socket.js
+++ src/helpers/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io("http://localhost:3300" || "https://taskflowbackend-cbxp.onrender.com", {
+const socket = io("https://taskflowbackend-cbxp.onrender.com", {
     withCredentials: true,
 })
 
